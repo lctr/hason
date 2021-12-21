04645b2cafe1580aecc91e66df8a85c4a5f322ce
Lictor Guzman <lctrgzmn@gmail.com>
2021-12-21 13:52:46 -0600

    parsing json

--- Parser.hs
+++ Parser.hs
@@ -3,6 +3,8 @@ module Parser where
 
 import           Control.Applicative
 import           Data.Char
+import           Util
+import           Value
 
 newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
 
@@ -37,12 +39,60 @@ char x = Parser $ \case
     parsed : rest | x == parsed -> Just (parsed, rest)
     _                           -> Nothing
 
-string :: String -> Parser String
-string = traverse char
+-- | TODO: explain
+symbol :: String -> Parser String
+symbol = traverse char
 
 -- | Ensure that a list of parsers is non-empty
 nonempty :: Parser [a] -> Parser [a]
 nonempty (Parser p) = Parser $ \s -> do
     (xs, rest) <- p s
     if null xs then Nothing else Just (xs, rest)
+
+-- | Parse internals of a string
+word :: Parser String
+word = satisfy (/= '"') <|> satisfy (/= '\"') <|> satisfy isEscapable
+
+string :: Parser String
+string = enclose '"' '"' word
+
+trim :: Parser a -> Parser a
+trim f = spaces *> f <* spaces
+
+enclose :: Char -> Char -> Parser a -> Parser a
+enclose left right f = char left *> f <* char right
+
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep item = (:) <$> item <*> many (trim sep *> item) <|> pure []
+
+parseNull :: Parser Value
+parseNull = Null <$ symbol "null"
+
+parseBool :: Parser Value
+parseBool = f <$> (symbol "true" <|> symbol "false")
+    where f x = Bool $ x == "true"
+
+parseNum :: Parser Value
+parseNum = f <$> nonempty (satisfy isDigit) where f ds = Num $ read ds
+
+parseStr :: Parser Value
+parseStr = Str <$> string
+
+parseArr :: Parser Value
+parseArr = Arr <$> (open *> trim elements <* close)
+  where
+    open     = char '[' *> spaces
+    close    = spaces <* char ']'
+    elements = sepBy (trim (char ',')) parseJson
+
+parseObj :: Parser Value
+parseObj = Obj <$> (open *> entry pair <* close)
+  where
+    open  = char '{' *> spaces
+    close = char '}' <* spaces
+    entry = sepBy (trim (char ','))
+    pair  = (\k _ v -> (k, v)) <$> string <*> trim (char ':') <*> parseJson
+
+parseJson :: Parser Value
+parseJson = parseNull <|> parseNum <|> parseStr <|> parseArr <|> parseObj
 
