d10616a9750c3868c8082f099a7e04b10b95873c
Lictor Guzman <lctrgzmn@gmail.com>
2021-12-21 10:02:27 -0600

    pretty printing character escapes

---
+++ Pretty.hs
@@ -0,0 +1,60 @@
+module Pretty where
+
+import           Data.Char
+
+import           Data.Bits
+import           Numeric
+
+data Doc = X
+    deriving Show
+
+instance Semigroup Doc where
+    a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+string :: String -> Doc
+string s = undefined
+
+text :: String -> Doc
+text t = undefined
+
+double :: Double -> Doc
+double d = undefined
+
+surround :: Char -> Char -> Doc -> Doc
+surround left right x = char left <> x <> char right
+
+-- | Concatenates multiple 'Doc' values, i.e., 'concat' for lists
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+-- DEALING WITH CHARACTERS AND ESCAPES
+character :: Char -> Doc
+character c = case lookup c simpleEscapes of
+    Just r -> text r
+    Nothing | mustEscape c -> hexEscape c
+            | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\', b])
+
+-- | Represent Unicode characters up to 0xffffff
+smallHex :: Int -> Doc
+smallHex x = text "\\u" <> text (replicate (4 - length h) '0') <> text h
+    where h = showHex x ""
+
+-- | Helper (bit-manipulation) function to represent characters with Unicode values above 0xfffff
+astral :: Int -> Doc
+astral n = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
+  where
+    a = (n `shiftR` 10) .&. 0x3ff
+    b = n .&. 0x3ff
+
+hexEscape :: Char -> Doc
+hexEscape c | d < 0x10000 = smallHex d
+            | otherwise   = astral (d - 0x10000)
+    where d = ord c
