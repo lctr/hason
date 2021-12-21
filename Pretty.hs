8978b5a2b4748ce0ca0bc6ecf5fdcf06d207c78d
Lictor Guzman <lctrgzmn@gmail.com>
2021-12-21 10:07:27 -0600

    strings; renamed some hex functions

--- Pretty.hs
+++ Pretty.hs
@@ -15,7 +15,7 @@ char :: Char -> Doc
 char c = undefined
 
 string :: String -> Doc
-string s = undefined
+string = surround '"' '"' . hcat . map character
 
 text :: String -> Doc
 text t = undefined
@@ -23,6 +23,7 @@ text t = undefined
 double :: Double -> Doc
 double d = undefined
 
+-- | Circumscribe with prefix and suffix
 surround :: Char -> Char -> Doc -> Doc
 surround left right x = char left <> x <> char right
 
@@ -32,14 +33,14 @@ hcat xs = undefined
 
 -- DEALING WITH CHARACTERS AND ESCAPES
 character :: Char -> Doc
-character c = case lookup c simpleEscapes of
+character c = case lookup c escaped of
     Just r -> text r
     Nothing | mustEscape c -> hexEscape c
             | otherwise    -> char c
     where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
 
-simpleEscapes :: [(Char, String)]
-simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+escaped :: [(Char, String)]
+escaped = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
     where ch a b = (a, ['\\', b])
 
 -- | Represent Unicode characters up to 0xffffff
@@ -48,13 +49,13 @@ smallHex x = text "\\u" <> text (replicate (4 - length h) '0') <> text h
     where h = showHex x ""
 
 -- | Helper (bit-manipulation) function to represent characters with Unicode values above 0xfffff
-astral :: Int -> Doc
-astral n = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
+bigHex :: Int -> Doc
+bigHex n = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
   where
     a = (n `shiftR` 10) .&. 0x3ff
     b = n .&. 0x3ff
 
 hexEscape :: Char -> Doc
 hexEscape c | d < 0x10000 = smallHex d
-            | otherwise   = astral (d - 0x10000)
+            | otherwise   = bigHex (d - 0x10000)
     where d = ord c
