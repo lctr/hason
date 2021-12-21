2ab6494fdc758ea819322edce7bc8c877b377347
Lictor Guzman <lctrgzmn@gmail.com>
2021-12-21 12:44:37 -0600

    added type signature for

--- Util.hs
+++ Util.hs
@@ -13,6 +13,7 @@ mustEscape :: Char -> Bool
 mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
 
 -- | If a character 'c' exists in the associated list of escapes, apply f to the resulting key value r; otherwise, if the character must be escaped regardless, apply g to c, otherwise apply h to c.
+escaped :: Char -> (String -> p) -> (Char -> p) -> (Char -> p) -> p
 escaped c f g h = case lookup c escapes of
     Just r -> f r
     Nothing | mustEscape c -> g c
